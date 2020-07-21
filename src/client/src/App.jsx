f6f2fc2f1baecd85fa3d6c88c77f42c81504870b
Abhishek9608 <abhishekkumar9608@gmail.com>
2020-07-21 15:24:48 +0530

    frontEnd

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import Routes from "./routes/Routes";
 
 function App() {
-  return <Route path="/" component={Routes} />;
+  return <h1>Home page</h1>;
 }
 
 export default App;
