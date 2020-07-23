d591567bae7ce4ca986052fd0ada6a7461ab990f
Abhishek9608 <abhishekkumar9608@gmail.com>
2020-07-23 14:38:19 +0530

    added:header

---
+++ src/client/src/components/productList/ProductList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+import Navbar from "../navigation/navbar";
+
+export default class ProductList extends Component {
+  render() {
+    return <Navbar />;
+  }
+}
--- src/client/src/components/routes/Routes.jsx
+++ src/client/src/components/routes/Routes.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 // import Home from "../components/Home";
-import AuthBanner from "../auth/AuthBanner";
+// import AuthBanner from "../auth/AuthBanner";
+import ProductList from "../productList/ProductList";
 
 export default class Routes extends Component {
   render() {
     return (
       <>
-        <Route path="/" component={AuthBanner} />
+        <Route path="/" component={ProductList} />
       </>
     );
   }
