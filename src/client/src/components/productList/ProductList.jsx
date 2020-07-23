7e17f8d04b3c592d7c81ba6b92472aca82563483
Abhishek9608 <abhishekkumar9608@gmail.com>
2020-07-23 22:11:44 +0530

    added component sidebar contentsection

--- src/client/src/components/productList/ProductList.jsx
+++ src/client/src/components/productList/ProductList.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import Navbar from "../navigation/navbar";
+import Sidebar from "./sidebar/Sidebar";
+import Contentsection from "../productList/contentSection/ContentSection";
 
 export default class ProductList extends Component {
   render() {
-    return <Navbar />;
+    return (
+      <div className="container-fluid p-0">
+        <Navbar />
+        <div className="row m-0">
+          <Sidebar />
+          <Contentsection />
+        </div>
+      </div>
+    );
   }
 }
