efc9c18f69c599fa8c3909dbaef4bd0ddcd62b7e
Abhishek9608 <abhishekkumar9608@gmail.com>
2020-07-23 22:09:51 +0530

    removed stick error

--- src/client/src/components/navigation/navbar.jsx
+++ src/client/src/components/navigation/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styles from "../navigation/Navbar.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 export default class navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-top border">
+      <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm  border">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/" style={{ padding: "0.5rem 0" }}>
             <img src="/images/logo_size.jpg" alt="EYO" style={{ height: "35px" }} />
@@ -19,7 +19,7 @@ export default class navbar extends Component {
             <ul className="navbar-nav ml-auto">
               <li role="presentation" className="nav-item d-none d-md-block d-lg-block d-xl-block">
                 <Link to="/" id={styles.button} className="nav-link btn  btn-sm font-weight-bold ">
-                  <FontAwesomeIcon icon={faUserCircle} />
+                  {/* <FontAwesomeIcon icon={faUserCircle} /> */}
                   Login / Signup
                 </Link>
               </li>
