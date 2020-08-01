eba7d03a1855f0ad2b9c0b64d1018b18a0645de9
Abhishek9608 <abhishekkumar9608@gmail.com>
2020-08-01 15:27:55 +0530

    ....

--- src/client/src/index.jsx
+++ src/client/src/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
