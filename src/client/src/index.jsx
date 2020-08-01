da6af81e9840af3ce0753fef686ad21be552427f
Rahul7744 <satish.b.s.kumar@gmail.com>
2020-08-01 17:04:44 +0530

    bug fixes

--- src/client/src/index.jsx
+++ src/client/src/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
